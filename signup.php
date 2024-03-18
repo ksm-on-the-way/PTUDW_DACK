c23b678ab03f40124c964987ed90a12206c5a2e2
KhanhQuynh <lnkquynh.2707@gmail.com>
2024-03-18 22:00:01 +0700

    signup

---
+++ signup.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng ký TIX ID</title>
+    <link rel="stylesheet" href="signup.css">
+    <body>
+        <div class="container">
+            <div class="left-section">
+                <img src="signup/left-chevron.png">
+                <a href="#" class="back-link">Trở về</a>
+            </div>
+            <div class="right-section">
+              <form class="signup-form">
+                    <h2>Đăng ký TIX ID</h2>
+
+                    <label for="username">TÀI KHOẢN</label>
+                    <input type="text" id="username" placeholder="Nhập tài khoản của bạn">
+
+                    <label for="pass">MẬT KHẨU</label>
+                    <input type="password" id="pass" placeholder="Nhập mật khẩu của bạn">
+
+                    <label for="pass1">NHẬP LẠI MẬT KHẨU</label>
+                    <input type="password" id="pass1" placeholder="Nhập lại mật khẩu của bạn">
+
+                    <label for="email">EMAIL</label>
+                    <input type="" id="email" placeholder="Nhập email của bạn">
+
+                    <button type="submit">Đăng ký ngay</button>
+                    <p>*Bằng việc đăng ký, tôi đồng ý và tuân theo các điều khoản và chính sách của TIX ID</p>
+                </form>
+                <p class="copyright">© 2024 TIX ID</p>
+            </div>
+        </div>
+    </body>
+</html>

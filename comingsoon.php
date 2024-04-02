5dea6e9bbaa4cb9418cc612db95f24109858f879
daokiet666 <kietfrancis@gmail.com>
2024-04-02 10:58:36 +0700

    Fetch using PHP (comingsoon + news article)

--- comingsoon.php
+++ comingsoon.php
@@ -1,10 +1,23 @@
 <?php
 include_once "header.php";
 ?>
-<html>
+<?php
+require_once("module.php");
+
+?>
+<?php
+echo '<html>
 <head>
     <link rel="stylesheet" href="comingsoon.css">
-    <script src="comingsoon.js"></script>
+    <script src="comingsoon.js">
+    // function showButtons() {
+    //     document.querySelector(".overlay").style.display = "flex";
+    // }
+    
+    // function hideButtons() {
+    //     document.querySelector(".overlay").style.display = "none";
+    // }
+    </script>
 </head>
 <body>
 <div class= "head">
@@ -12,83 +25,47 @@ include_once "header.php";
     <div class="comingsoon_subtitle">Chờ đợi chúng đến rạp chiếu phim yêu thích của bạn!</div>
 </div>
 <div class="comingsoon_film_container">
-  <div class="comingsoon_items">
-    <div class="column" id="hehe">
+  <div class="comingsoon_items">';
+$link = NULL;
+taoKetNoi($link);
+$result=chayTruyVanTraVeDL($link,"SELECT * FROM movie WHERE release_date > CURDATE() ORDER BY release_date ASC");
+while($rows=mysqli_fetch_assoc($result)){
+echo '
+    <div class="column">
+        <div class = "img_container">
         <img
           loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
+          srcset="'.$rows['movie_banner_src'].'"
           class="img"
         />
-        <div class="comingsoon_film_title">The Matrix: Resurections</div>
+        <div class="overlay" onmouseover="showButtons()" onmouseout="hideButtons()">
+        <a href="page1.php" class="button">Button 1</a>
+        <a href="page2.php" class="button">Button 2</a>
+        </div>
+        </div>    
+        <div class="content">
+        <div class="comingsoon_film_title">'.$rows['movie_name'].'</div>
         <div class = "comingsoon_info_container">
         <div class="comingsoon_film_info">
-            <span class = "bold_info">Thể loại: </span><span class = "info">hi</span>
+            <span class = "bold_info">Thể loại: </span><span class = "info">'.$rows['movie_genre'].'</span>
         </div>
         <div class="comingsoon_film_info">
-            <span class = "bold_info">Thời lượng: </span><span class = "info"></span>
+            <span class = "bold_info">Thời lượng: </span><span class = "info">'.$rows['movie_duration'].'</span>
         </div>
         <div class="comingsoon_film_info">
-            <span class = "bold_info">Ngày khởi chiếu: </span><span class = "info"></span>
+            <span class = "bold_info">Ngày khởi chiếu: </span>'.$rows['release_date'].'<span class = "info"></span>
         </div>
         </div>
+        </div>  
     </div>
-    <div class="column">
-        <img
-          loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/7aa0b036254ec04303b65852e71d2ed87752a18a14c612e76ecefbb8a7d5e1e8?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
-          class="img"
-        />
-        <div class="comingsoon_film_title">Resident Evil: Welcome to Raccoon City</div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Thể loại: </span><span class = "info">hi</span>
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Thời lượng: </span><span class = "info"></span>
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Ngày khởi chiếu: </span><span class = "info"></span>
-        </div>
-    </div>
-    <div class="column">
-        <img
-          loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/6cb0be60e90d0074074a0cd0802ddcc344db6aa5480dd5f3b8594790abd15a73?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
-          class="img"
-        />
-        <div class="comingsoon_film_title">
-          Sword Art Online: Progressive - Aria of a Starless Night
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Thể loại: </span><span class = "info">hi</span>
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Thời lượng: </span><span class = "info"></span>
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Ngày khởi chiếu: </span><span class = "info"></span>
-        </div>
-    </div>
-    <div class="column">
-        <img
-          loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/f979c0f8e3befe2d5a01fa841e5a9ac7cdde6c80df7d354a93710b6c78c81d6f?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
-          class="img"
-        />
-        <div class="comingsoon_film_title">hehe</div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Thể loại: </span><span class = "info">hi</span>
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Thời lượng: </span><span class = "info"></span>
-        </div>
-        <div class="comingsoon_film_info">
-            <span class = "bold_info">Ngày khởi chiếu: </span><span class = "info"></span>
-        </div>
-    </div>
-  </div>
+  ';
+}
+echo '</div>
 </div>
 
 </body>
-</html>
+</html>';
+giaiPhongBoNho($link, $result);
+?>
 
 
--- news_article.php
+++ news_article.php
@@ -1,20 +1,17 @@
 <?php
-include_once "header.php";
-?>
-<?php
 require_once "module.php";
 if(isset ($_GET["id"])) {
     $article_id = $_GET["id"];
     $link = null;
     taoKetNoi($link);
-    $sql = "SELECT * FROM tbl_news WHERE id =".$article_id;
+    $sql = "SELECT * FROM news WHERE newsid =".$article_id;
     $result = chayTruyVanTraVeDL($link, $sql);
-    $rows=mysqli_fetch_row($result);
+if(mysqli_num_rows($result)!=0) {
+    include_once "header.php";
+    $rows=mysqli_fetch_assoc($result);
     giaiPhongBoNho($link, $result);
-}
-else { echo "Error 404";}
-?>
-<!DOCTYPE html>
+echo
+'<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -24,90 +21,62 @@ else { echo "Error 404";}
 <div class="container">
   <div class = "first_container">
   <div class="maininfo">
-      <div class="news_title">Spider-Man: No Way Home Rilis Trailer Terbaru</div>
-      <div class="upload_date">17 Nov 2021 | TIX ID</div>
+      <div class="news_title">'.$rows['news_title'].'</div>
+      <div class="upload_date">'.$rows['news_date'].'</div>
   </div>
   <img
     loading="lazy"
-    srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/d952155a1f4eaf980351e1eb14cef8adfefafd33abadceeb21166e12ffc58776?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
+    srcset = '.$rows['news_banner_src'].'
     class="news_img"
   />
-  <div class="news_content">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Laoreet quis vitae
-    molestie eleifend in. Proin volutpat id congue diam. Gravida lorem augue
-    senectus nunc et, sagittis in mattis feugiat. Pharetra eleifend eget
-    pellentesque faucibus laoreet. Fames amet hac quis suscipit proin amet.
-    Neque rutrum nam parturient ac. Egestas ut vestibulum ac diam odio malesuada
-    potenti. Donec vitae consequat consequat ornare ante et pulvinar. Diam vitae
-    neque ridiculus diam at posuere volutpat. Donec in pellentesque diam congue.
-    <br />
-    <br />
-    Ultrices malesuada diam condimentum risus. In velit justo eu ac amet fusce
-    lorem urna. Quis sed neque sed duis. Eleifend purus nam at cras nisi, vitae.
-    Eleifend mollis sem odio morbi faucibus. Adipiscing in libero pharetra odio
-    quam. Suspendisse tortor, viverra odio ultrices.
-    <br />
-    Eu arcu odio neque malesuada ut blandit sit. In justo, suspendisse sit
-    faucibus morbi egestas ut facilisis egestas. Turpis non amet massa a, elit,
-    in. Lectus at elementum, a nullam in. Commodo magna senectus malesuada ut
-    rhoncus in. Placerat arcu congue faucibus auctor purus, fringilla praesent
-    maecenas
-    <br />
-    <br />
-    Quis sed lobortis sed amet nec eu, dolor. Elementum in integer sagittis
-    tellus scelerisque imperdiet felis sit mauris. Scelerisque nunc, nullam
-    malesuada leo odio malesuada lobortis egestas. Neque at fringilla morbi
-    nulla facilisi tellus sit lobortis cursus. Venenatis at aliquet auctor ut
-    elit, urna. Consequat quis risus turpis amet.
-  </div>
+  <div class="news_content">'.$rows['news_content'].'</div>
   <div class="other_article_txt">Xem thêm các bài viết khác</div>
-  </div>
-  
+  </div>';
+  $link = null;
+  taoKetNoi($link);
+  $sql = "SELECT * FROM news WHERE newsid != $article_id ORDER BY news_date DESC";
+  $result = chayTruyVanTraVeDL($link, $sql);
+  // mysqli_num_rows($result);
+  echo '
   <div class="other_news">
-  <div class="news_items">
-    <div class="column" id="column_1">
+  <div class="news_items">';
+  for ($x=1; $x<=3;$x++) {
+    $rows=mysqli_fetch_assoc($result);
+    echo '
+    <div class="column" onclick="navigateToArticle('.$rows['newsid'].')">
         <img
           loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ef2a30bb30cd1d60eb924a537f9a3c3bb70a5a76c553fb3532a04139a0017ad7?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
+          srcset="'.$rows['news_banner_src'].'"
           class="img"
         />
         <div class ="otnews_content">
-        <div class = "news_category">News</div>
+        <div class = "news_category">'.$rows['news_category'].'</div>
         <div class="other_news_title">
-          Ghostbusters: Afterlife Hadir Menampilkan Variasi Hantu Baru
+        '.$rows['news_title'].'
         </div>
-        <div class="other_news_date">08 Nov 2021 | TIX ID</div>
+        <div class="other_news_date">'.$rows['news_date'].'</div>
         </div>
-    </div>
-    <div class="column" id="column_2">
-        <img
-          loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa9b3e0530843484631008c865366b1fdd6998bc11f043e4fea3f7f8619f471e?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
-          class="img"
-        />
-        <div class="other_news_title">
-          House of Gucci: Kisah Pewaris Tunggal Gucci Pada Tahun 1955.
-        </div>
-        <div class="other_news_date">09 Nov 2021 | TIX ID</div>
-    </div>
-    <div class="column" id="column_3">
-        <img
-          loading="lazy"
-          srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ed23c4feb0151894ad2b8a0b0696102fde9b1b793fa0f9b8f52746aacc458501?apiKey=26bc666b112f4cabba6b77c16b9bc389&"
-          class="img"
-        />
-        <div class="other_news_title">
-          Aksi Donnie Yen Dalam Film Aksi Hongkong Terbaru
-        </div>
-        <div class="other_news_date">15 Nov 2021 | TIX ID</div>
-    </div>
-  </div>
-</div>
+    </div>';
+  };
+  echo '<script>
+        function navigateToArticle(articleId) {
+            // window.location.href = "news_article.php?id=" + articleId;
+            const column = document.querySelector(".column");
+            column.style.transition = "transform 0.5s ease-in-out";
+            column.style.transform = "scale(0.9)"; 
+            setTimeout(() => {
+              window.location.href = "news_article.php?id=" + articleId;
+            }, 50); 
+        }
+      </script>';
+echo '</div>
 </div>
 
 </body>
-</html>
-
-<?php
+</html>';
 include_once "footer.php";
+}
+else { echo "Error 404";}
+}
+else { echo "Error 404";}
 ?>
